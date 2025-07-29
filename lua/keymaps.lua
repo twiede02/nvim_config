9f9844f96211fa6f8d86ef9113e8ddfdaa850c00
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2025-07-29 15:40:58 +0200

    lines

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Live grep" })
 vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Search buffers" })
 vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Help tags" })
 vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>", { desc = "Toggle Neo-Tree" })
+
+
+vim.keymap.set("n", "<S-l>", "<Plug>(cokeline-focus-next)", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-h>", "<Plug>(cokeline-focus-prev)", { desc = "Prev buffer" })
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional but recommended
+  config = function()
+    require("lualine").setup()
+  end,
+}
+
